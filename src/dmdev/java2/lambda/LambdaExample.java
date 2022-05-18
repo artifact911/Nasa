7904a935ff0510b9baacbb1b9e395f34b3b1a390
artifact911 <artifact911@gmail.com>
2022-05-18 14:55:24 +0300

    straem done

--- src/dmdev/java2/lambda/LambdaExample.java
+++ src/dmdev/java2/lambda/LambdaExample.java
@@ -1,5 +1,21 @@
 package dmdev.java2.lambda;
 
+import java.util.Comparator;
+
 public class LambdaExample {
 
+    public static void main(String[] args) {
+
+/*        Comparator<Integer> comparator = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return 0;
+            }
+        };*/
+        // перепишем с возможностями лямбды
+
+        Comparator<Integer> comparator = Integer::compare;
+
+        System.out.println(comparator.compare(25, 100));
+    }
 }
---
+++ src/dmdev/java2/lambda/StreamExample.java
@@ -0,0 +1,30 @@
+package dmdev.java2.lambda;
+
+import java.util.Collections;
+import java.util.List;
+
+public class StreamExample {
+
+    public static void main(String[] args) {
+        List<String> strings = List.of("88", "11", "22", "33", "44", "55", "66");
+       /* for (String string : strings) {
+            String value = string + string;
+            Integer intValue = Integer.valueOf(value);
+            if(intValue % 2 == 0) {
+                System.out.println(intValue);
+            }
+        }*/
+
+        // перепишем через Stream
+        strings.stream()
+                .map((val) -> val + val)
+//                .map(val -> Integer.valueOf(val))
+        // заменим на лямбду
+                .map(Integer::valueOf)
+                .filter(val -> val % 2 == 0)
+                .sorted()
+                .skip(1)
+                .limit(2)
+                .forEach(System.out::println);
+    }
+}
