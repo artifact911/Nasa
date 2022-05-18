b03f273e4ec47e55d6a047d2108898c143dcf07e
artifact911 <artifact911@gmail.com>
2022-05-18 17:46:06 +0300

    MapReduce done

---
+++ src/dmdev/java2/lambdastream/MapReduceExample.java
@@ -0,0 +1,36 @@
+package dmdev.java2.lambdastream;
+
+import java.util.stream.Stream;
+
+public class MapReduceExample {
+
+    /**
+     * [1000_000] -> age -> max
+     *
+     * [1000_000] -> age -> max -> max
+     *
+     * [1000_000] -> age -> max
+     *
+     */
+
+    public static void main(String[] args) {
+        Stream.of(
+                new Student(18, "Ivan"),
+                new Student(23, "Petr"),
+                new Student(34, "Vasiliy"),
+                new Student(45, "Svetlana"),
+                new Student(20, "Katya"),
+                new Student(68, "Den"),
+                new Student(101, "Kira")
+        )
+                .parallel() // запустили параллельно
+//                .sequential() // свернет в один поток
+                .map(Student::getAge)
+//                .reduce((age1, age2) -> Math.max(age1, age2))
+//                .reduce(Math::max)
+//                .reduce(Math::max)
+//                .reduce(0, Integer::sum)
+                .reduce(Integer::sum)
+                .ifPresent(System.out::println);
+    }
+}
