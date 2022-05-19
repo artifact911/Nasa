97f6f5237f0f13d9c52aba7a48fdd8b478fbcbc9
artifact911 <artifact911@gmail.com>
2022-05-19 18:35:52 +0300

    Stream Task2 done

--- src/dmdev/java2/lambdastream/practice/Task1.java
+++ src/dmdev/java2/lambdastream/practice/Task1.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.OptionalDouble;
 
 /**
- *  Дан список целых чисел. Найти среднее всех нечетных чисел, делящихся на 5.
+ * Дан список целых чисел. Найти среднее всех нечетных чисел, делящихся на 5.
  */
 public class Task1 {
 
@@ -12,20 +12,31 @@ public class Task1 {
         List<Integer> integers = List.of(1, 3, 4, 6, 8, 5, 15, 20, 25, 10);
 
         double average = integers.stream()
-                                  .filter(i -> i % 2 != 0)
-                                  .filter(i -> i % 5 == 0)
-                                  .mapToInt(Integer::intValue)
-                                  .average()
-                                  .getAsDouble();
+                                 .filter(i -> i % 2 != 0)
+                                 .filter(i -> i % 5 == 0)
+                                 .mapToInt(Integer::intValue)
+                                 .average()
+                                 .getAsDouble();
+
         System.out.println(average);
 
 
-       // не мой варик
+        // не мой варик
         OptionalDouble average1 = integers.stream()
                                           .filter(i -> i % 2 != 0)
                                           .filter(i -> i % 5 == 0)
                                           .mapToInt(Integer::intValue)
                                           .average();
+
         average1.ifPresent(System.out::println);
+
+        //варик от Кирьянушки
+        double average3 = integers.stream()
+                                  .filter(i -> i % 2 != 0)
+                                  .filter(i -> i % 5 == 0)
+                                  .mapToInt(Integer::intValue)
+                                  .summaryStatistics().getAverage();
+
+        System.out.println(average3);
     }
 }
---
+++ src/dmdev/java2/lambdastream/practice/Task2.java
@@ -0,0 +1,30 @@
+package dmdev.java2.lambdastream.practice;
+
+import java.util.List;
+
+/**
+ * Дан список строк. Найти количество уникальных строк длиной более 8 символов.
+ */
+
+public class Task2 {
+
+    public static void main(String[] args) {
+        List<String> strings = List.of(
+                "string-1",
+                "string-2",
+                "string-3",
+                "string-4",
+                "string-10",
+                "string-10",
+                "string-10",
+                "string-12",
+                "string-16"
+        );
+
+        long count = strings.stream()
+                            .filter(s -> s.length() > 8)
+                            .distinct()
+                            .count();
+        System.out.println(count);
+    }
+}
