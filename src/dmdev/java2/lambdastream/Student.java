3ab1d13991b5132eee88d1620794febc1c5499cb
artifact911 <artifact911@gmail.com>
2022-05-19 17:21:39 +0300

    Optional done

--- src/dmdev/java2/lambdastream/Student.java
+++ src/dmdev/java2/lambdastream/Student.java
@@ -1,9 +1,13 @@
 package dmdev.java2.lambdastream;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student {
 
     private int age;
     private String name;
+    private List<Integer> marks = new ArrayList<>();
 
     public Student(int age, String name) {
         this.age = age;
@@ -16,5 +20,17 @@ public class Student {
 
     public String getName() {
         return name;
+    }
+
+    public List<Integer> getMarks() {
+        return marks;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
---
+++ src/dmdev/java2/optionalex/OptionalEx.java
@@ -0,0 +1,39 @@
+package dmdev.java2.optionalex;
+
+import dmdev.java2.lambdastream.Student;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class OptionalEx {
+
+    public static void main(String[] args) {
+
+        Stream<Student> studentStream = Stream.of(
+                new Student(18, "Ivan"),
+                new Student(23, "Petr"),
+                new Student(34, "Vasiliy"),
+                new Student(45, "Svetlana"),
+                new Student(20, "Katya"),
+                new Student(68, "Den"),
+                new Student(101, "Kira")
+        );
+
+        Optional<Student> maybeStudent = studentStream
+                .sequential() // свернет в один поток
+             /*
+                .flatMap(student -> student.getMarks().stream()) // flatMap может вернуть другой стрим
+                .map(mark -> mark.intValue())*/
+//                                               .map(Student::getAge)
+                .reduce((student1, student2) -> student1.getAge() > student2.getAge() ? student1 : student2);
+
+//        maybeStudent.ifPresent(System.out::println);
+
+        maybeStudent.map(Student::getAge)
+//                .filter(age -> age > 25)
+//                .flatMap(age -> Optional.of(age * 2))
+                    .flatMap(age -> Optional.ofNullable(null))// в flatMap мы можем юзать др функции и вернуть Optional
+                    .ifPresent(System.out::println); // этот метод безопасен к null и можно не проверять
+
+    }
+}
