49a2b21367a46e412f032348d29098c2d1c73bbf
artifact911 <artifact911@gmail.com>
2022-05-19 18:05:32 +0300

    Stream Task1 done

--- src/dmdev/java2/lambdastream/practice/Task1.java
+++ src/dmdev/java2/lambdastream/practice/Task1.java
@@ -1,5 +1,31 @@
 package dmdev.java2.lambdastream.practice;
 
+import java.util.List;
+import java.util.OptionalDouble;
+
+/**
+ *  Дан список целых чисел. Найти среднее всех нечетных чисел, делящихся на 5.
+ */
 public class Task1 {
 
+    public static void main(String[] args) {
+        List<Integer> integers = List.of(1, 3, 4, 6, 8, 5, 15, 20, 25, 10);
+
+        double average = integers.stream()
+                                  .filter(i -> i % 2 != 0)
+                                  .filter(i -> i % 5 == 0)
+                                  .mapToInt(Integer::intValue)
+                                  .average()
+                                  .getAsDouble();
+        System.out.println(average);
+
+
+       // не мой варик
+        OptionalDouble average1 = integers.stream()
+                                          .filter(i -> i % 2 != 0)
+                                          .filter(i -> i % 5 == 0)
+                                          .mapToInt(Integer::intValue)
+                                          .average();
+        average1.ifPresent(System.out::println);
+    }
 }
