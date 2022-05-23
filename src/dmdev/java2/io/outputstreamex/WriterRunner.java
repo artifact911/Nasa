93b406a16f3ecd3b343e6fe4ba73b969bb5442f0
artifact911 <artifact911@gmail.com>
2022-05-23 10:44:58 +0300

    IO Writer and BufferedWriter.class done

---
+++ src/dmdev/java2/io/outputstreamex/WriterRunner.java
@@ -0,0 +1,24 @@
+package dmdev.java2.io.outputstreamex;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+
+public class WriterRunner {
+
+    public static void main(String[] args) throws IOException {
+        File file = Path.of("src", "dmdev", "java2", "io", "resources", "writer.txt").toFile();
+
+        // Если мы хотим дописывать, а не перезаписывать, то нужно написать так: FileOutputStream outputStream = new FileOutputStream
+        // (file, true)
+        try (BufferedWriter fileWriter = new BufferedWriter(new FileWriter(file, true))) {
+            fileWriter.append("Hello World!");
+            fileWriter.newLine();
+            fileWriter.append("Java");
+
+        }
+    }
+
+}
