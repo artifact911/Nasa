9e4fe5b9c996c727681978813a4891f498b0043f
artifact911 <artifact911@gmail.com>
2022-05-25 14:07:45 +0300

    Thread.getState() done

--- src/dmdev/java2/thread/ThreadDemo.java
+++ src/dmdev/java2/thread/ThreadDemo.java
@@ -7,16 +7,21 @@ public class ThreadDemo {
 
     public static void main(String[] args) {
         var simpleThread = new SimpleThread();
+        // узнаем состояние потока
+        Thread.State state = simpleThread.getState();
+        System.out.println(simpleThread.getName() + ": " + simpleThread.getState());
         var runnableThread = new Thread(new SimpleRunnable(), "My name");
         var lambdaThread = new Thread(() -> System.out.println("Hello from lambda: " + Thread.currentThread().getName()));
 
         // не run()!!!
         simpleThread.start();
+        System.out.println(simpleThread.getName() + ": " + simpleThread.getState());
         runnableThread.start();
         lambdaThread.start();
         try {
             // этот метод говорит, что нужно завершить все действия этого потока до того, как мы перейдем к другому
-            simpleThread.join();
+            simpleThread.join(100L);
+            System.out.println(simpleThread.getName() + ": " + simpleThread.getState());
             runnableThread.join();
             lambdaThread.join();
         } catch (InterruptedException e) {
@@ -27,9 +32,5 @@ public class ThreadDemo {
 
         // узнали название потока main
         System.out.println(Thread.currentThread().getName());
-
-
-
-
     }
 }
