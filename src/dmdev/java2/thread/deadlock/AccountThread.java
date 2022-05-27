d47bbe87a22156537714d3d21f58df7d4545d8e6
artifact911 <artifact911@gmail.com>
2022-05-27 20:23:38 +0300

    deadlock done

---
+++ src/dmdev/java2/thread/deadlock/AccountThread.java
@@ -0,0 +1,27 @@
+package dmdev.java2.thread.deadlock;
+
+public class AccountThread extends Thread {
+
+    private final Account accountFrom;
+    private final Account accountTo;
+
+    public AccountThread(Account accountFrom, Account accountTo) {
+        this.accountFrom = accountFrom;
+        this.accountTo = accountTo;
+    }
+
+    @Override
+    public void run() {
+
+        for (int i = 0; i < 20; i++) {
+            synchronized (accountFrom) {
+                synchronized (accountTo) {
+                    if (accountFrom.takeOff(10)) {
+                        accountTo.add(10);
+                    }
+
+                }
+            }
+        }
+    }
+}
