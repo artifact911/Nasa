2f0dacd2b453b8e65b3acc8429458396dc9ab44f
artifact911 <artifact911@gmail.com>
2025-02-24 18:11:08 +0300

    Revert "testCommit"
    This reverts commit e1815f9e4db8ca974a24cd9cd1396bd45edf6652.

--- src/multithread/vzuev/lessons/run25_Condition/BoundedBuffer25.java
+++ src/multithread/vzuev/lessons/run25_Condition/BoundedBuffer25.java
@@ -17,7 +17,6 @@ import static java.util.stream.Collectors.joining;
  */
 public final class BoundedBuffer25<T> {
 
-
     private final T[] items;
     private int size;
     private final Lock lock;
